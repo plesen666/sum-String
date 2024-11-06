c4a278933f068783425582f9626b935fab456675
Екатерина <diada.var@mail.ru>
2024-11-06 19:31:12 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int sum = 0;
-        String temp= "0";
-        String str ="1,2,3,4,5,6,7,8,9,10";
-        char ch= str.charAt(0);
-
-
-        for (int i = 0; i < str.length() ; i++) {
-
-        if (Character.isDigit(str.charAt(i))) {
-
-           sum += str.charAt(i);
-
-
-        } else {
-
-            sum += Integer.parseInt(temp);
-
-            temp = "0";
+        int sum = 0;//хранения суммы всех найденных цифр.
+        String str = "0,1,2,3,4,5,6,7,8,9";
+        for (int i = 0; i < str.length(); i++) { //проходимся по всем символам строки
+            char ch = str.charAt(i); //Программа берет символ из строки str, который находится на позиции i, и сохраняет его в переменной ch
+            if (Character.isDigit(ch)) {
+                int digit = Character.getNumericValue(ch);
+                sum = sum + digit;
+            }
         }
-    }
         System.out.println(sum);
-
-
     }
 }
